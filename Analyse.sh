a49e0ff0ce489833bb6a450988738ec93d34cd8c
Dorexo <dorian.rena@laposte.net>
2024-11-24 16:46:22 -0500

    Start

--- Analyse.sh
+++ Analyse.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-source /home/dodo/Documents/UQAC/SécuInfo_Projet/venv/bin/activate
 python3 /home/dodo/Documents/UQAC/SécuInfo_Projet/main_ui.py
--- AnalyseDownload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir /home/dodo/Documents/UQAC/SécuInfo_Projet/test
-source /home/dodo/Documents/UQAC/SécuInfo_Projet/venv/bin/activate
-python3 /home/dodo/Documents/UQAC/SécuInfo_Projet/main_analyse.py
